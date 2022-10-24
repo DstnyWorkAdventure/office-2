6d8a3af9a6282f129eb78fdfae699fc28a9a36c3
Amaury Demilie <ade@escaux.com>
2022-10-24 02:16:21 +0200

    adding pngs

---
+++ t/RPGMakerVXAce/japan2.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="japan2" tilewidth="512" tileheight="512" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="512" source="GK_JO_B.png"/>
+ </tile>
+ <tile id="1">
+  <image width="512" height="512" source="GK_JO_C.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="GK_JO_A2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="512" source="GK_JO_A5.png"/>
+ </tile>
+</tileset>
