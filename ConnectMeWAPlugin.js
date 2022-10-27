1c1bf86247696bf8e8eb8c3b799fca0ad063a8a2
Amaury Demilie <ade@escaux.com>
2022-10-27 13:28:26 +0200

    a

---
+++ ConnectMeWAPlugin.js
@@ -0,0 +1,41 @@
+
+//Needed to be able to get properties easily
+import {} from "https://unpkg.com/@workadventure/scripting-api-extra@1.4.4/dist/bundle.js"
+
+
+//parent.postMessage("myevent", "*")
+//console.log("sending event")
+
+WA.onInit().then(() => {
+    console.log('Current player name: ', WA.player.name);
+});
+
+const map = await WA.room.getTiledMap();
+
+WA.player.onPlayerMove(console.log);
+
+const myAdminOffice1 = WA.room.area.onEnter("AdminOffice1").subscribe(() => {
+  const command = {action: "setStatus", body: "available"};
+  parent.postMessage(JSON.stringify(command), "*");
+});
+
+WA.room.area.onLeave("AdminOffice1").subscribe(() => {
+
+  const command = {action: "setStatus", body: "out_of_office"};                                                                                                                                                        
+  parent.postMessage(JSON.stringify(command), "*");       
+});
+
+const myAdminOffice2 = WA.room.area.onEnter("AdminOffice2").subscribe(() => {                                                                                                                                      
+  const command = {action: "setStatus", body: "meeting"};                                                                                                                                                        
+  parent.postMessage(JSON.stringify(command), "*");   
+  const command2 = {action: "setMood", body: "In Green room"};
+  parent.postMessage(JSON.stringify(command2), "*");                                                                                                                                                             
+});                                                                                                                                                                                                                
+                                                                                                                                                                                                                   
+WA.room.area.onLeave("AdminOffice2").subscribe(() => {                                                                                                                                                             
+                                                                                                                                                                                                                   
+  const command = {action: "setStatus", body: "available"};                                                                                                                                                    
+  parent.postMessage(JSON.stringify(command), "*");                                                                                                                                                                
+  const command2 = {action: "setMood", body: "---"};                                                                                                                                                     
+  parent.postMessage(JSON.stringify(command2), "*");          
+});
