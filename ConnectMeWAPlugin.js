7c10dda8799a0d6a491c44bc69b9972cd4a087fa
Amaury Demilie <ade@escaux.com>
2022-10-30 10:04:50 +0100

    a

--- ConnectMeWAPlugin.js
+++ ConnectMeWAPlugin.js
@@ -33,7 +33,7 @@ const myCollide = WA.room.area.onEnter("Collide").subscribe(() => {
   WA.controls.disablePlayerControls()
   setTimeout(() => {
     console.log("Delayed for 400ms");
-    WA.controls.enablePlayerControls()
+    WA.controls.restorePlayerControls()
   }, 400);
   WA.player.getPosition().then((position) => {
   console.log(position)
